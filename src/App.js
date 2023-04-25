035e9964884c60ef622dbc6c07ae685df89407cb
Ben Heyman <benheyman1@gmail.com>
2023-04-25 17:09:26 +0100

    Add hero page with svg img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 import BrandVideo from "./components/BrandVideo";
+import Hero from "./components/Hero";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
       <BrandVideo />
     </div>
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Hero.css'
+import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+
+export default function Hero() {
+  return (
+    <div className='Container'>
+      <div className="HeroItems">
+        <img src={require('../hero.svg').default} alt='mySvgImage' className='HeroSVG' />
+        <p className='InvestmentText'>
+          We are investing $3 million over the next three years in our <br />
+          Build|Grow|Serve program, created to support and empower Black and <br />
+          underrepresented communities
+        </p>
+        <Button
+          className='Button-LearnMore'
+          variant="contained"
+          style={{ backgroundColor: '#ffa98e' }}
+          endIcon={<ArrowForwardIcon />}
+          sx={{
+            borderRadius: 50
+          }}
+        >
+          Learn More
+        </Button>
+      </div>
+    </div>
+  )
+}
