92783d13899d4da9ef2c0d0236efdf655ca4349a
Ben Heyman <benheyman1@gmail.com>
2023-04-25 14:48:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import BrandVideo from "./components/BrandVideo";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BrandVideo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BrandVideo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BrandVideo() {
+  return (
+    <div>BrandVideo</div>
+  )
+}
