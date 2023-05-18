847fa828d6b1758b3f286831d071838e71d3b840
Ben Heyman <benheyman1@gmail.com>
2023-05-18 14:29:35 +0100

    Add video placeholder and text below

--- src/components/BrandVideo.js
+++ src/components/BrandVideo.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import './BrandVideo.css'
 
 export default function BrandVideo() {
   return (
-    <div>BrandVideo</div>
+    <div className='VideoContainer'>
+      <img src="./img/video-placeholder.png" alt="" />
+      <div className="ConnectedBrandsText">
+        <h2>We build connected brand systems from vision through execution</h2>
+      </div>
+    </div>
   )
 }
